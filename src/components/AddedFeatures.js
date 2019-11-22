f601f5ddb117997f058673c5dee41c73ef46f3e5
adkhiker <poirier.greg@gmail.com>
2019-11-21 19:10:47 -0500

    WIP: added car db and reducer

--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -6,10 +6,10 @@ const AddedFeatures = props => {
   return (
     <div className="content">
       <h6>Added features:</h6>
-      {props.car.features.length ? (
+      {props.car.car.features.length ? (
         <ol type="1">
-          {props.car.features.map(item => (
-            <AddedFeature key={item.id} feature={item} />
+          {props.car.car.features.map(item => (
+            <AddedFeature key={item.id} feature={item} deleteFeature={props.deleteFeature} car={props.car}/>
           ))}
         </ol>
       ) : (
