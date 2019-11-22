60a82a2ff26efb46744aabc9f86f91ba670d1ed1
adkhiker <poirier.greg@gmail.com>
2019-11-21 19:20:42 -0500

    WIP:Styling and cleaning code

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
-import React from 'react';
-import Car from './components/Car';
-import { connect } from 'react-redux';
-import { Route, Link } from 'react-router-dom';
-import {addFeature} from './actions/featuresActions';
-import {deleteFeature} from './actions/featuresActions';
-const App = (props) => {
-  console.log(props)
+import React from "react";
+import Car from "./components/Car";
+import { connect } from "react-redux";
+import { Route, Link } from "react-router-dom";
+import { addFeature } from "./actions/featuresActions";
+import { deleteFeature } from "./actions/featuresActions";
+const App = props => {
+  console.log(props);
   const removeFeature = (item, car) => {
     // dispatch an action here to remove an item
-    props.deleteFeature(item, car)
+    props.deleteFeature(item, car);
   };
 
   const buyItem = (item, car) => {
     // dipsatch an action here to add an item
-    props.addFeature(item, car)
+    props.addFeature(item, car);
   };
-
 
   return (
     <div>
-    {props.cars.map(car => {
-      return <Link to={`/${car.car.url}`}><button>{car.car.name}</button></Link>
-    })}
-    {props.cars.map(car => {
-      return <Route path={`/${car.car.url}`} render={(props) => <Car car={car} removeFeature={removeFeature} buyItem={buyItem}/>} />
-    })}
-    <Route path='/' exact />
+      {props.cars.map(car => {
+        return (
+          <Link to={`/${car.car.url}`}>
+            <button>{car.car.name}</button>
+          </Link>
+        );
+      })}
+      {props.cars.map(car => {
+        return (
+          <Route
+            path={`/${car.car.url}`}
+            render={props => (
+              <Car car={car} removeFeature={removeFeature} buyItem={buyItem} />
+            )}
+          />
+        );
+      })}
+      <Route path="/" exact />
     </div>
   );
 };
@@ -33,14 +43,7 @@ const App = (props) => {
 const mapStateToProps = state => {
   return {
     cars: state.cars
-  }
-}
+  };
+};
 
-export default connect(
-  mapStateToProps,
-  {addFeature, deleteFeature }
-)(App);
-
-
-
-
+export default connect(mapStateToProps, { addFeature, deleteFeature })(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'react-redux';
-import {createStore } from 'redux'
-import { BrowserRouter as Router} from 'react-router-dom'
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bulma/css/bulma.css";
+import "./styles.scss";
 
-import { featuresReducer } from './reducer/featuresReducer';
+import { featuresReducer } from "./reducer/featuresReducer";
 export const store = createStore(featuresReducer);
-const rootElement = document.getElementById('root');
-ReactDOM.render(<Provider store={store}><Router><App /></Router></Provider>, rootElement);
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  rootElement
+);
